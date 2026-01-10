3b0d21c7641a1f9ae4b7c68f42d7d8f817b99ee9
sliomot <sliomot0506@gmail.com>
2026-01-10 19:34:40 +0100

    fix: never read ProyectCard & proyects

--- src/components/proyects.tsx
+++ src/components/proyects.tsx
@@ -1,22 +1,22 @@
-import { ProyectCard } from "./proyectscard";
-const proyects = [
-  {
-    title: "Proyects Name Placeholder",
-    img: "https://placehold.co/150x150",
-    description:
-      "Ponyta is very weak at birth. It can barely stand up. This Pokémon becomes stronger by stumbling and falling to keep up with its parent.It’s proud of its thick horn.",
-    direction: "#",
-    technologies: ["JavaScript", "React", "Java"],
-  },
-  {
-    title: "Proyects Name Placeholder",
-    img: "https://placehold.co/150x150",
-    description:
-      "Ponyta is very weak at birth. It can barely stand up. This Pokémon becomes stronger by stumbling and falling to keep up with its parent.It’s proud of its thick horn.",
-    direction: "#",
-    technologies: ["JavaScript", "React", "Java"],
-  },
-];
+// import { ProyectCard } from "./proyectscard";
+// const proyects = [
+//   {
+//     title: "Proyects Name Placeholder",
+//     img: "https://placehold.co/150x150",
+//     description:
+//       "Ponyta is very weak at birth. It can barely stand up. This Pokémon becomes stronger by stumbling and falling to keep up with its parent.It’s proud of its thick horn.",
+//     direction: "#",
+//     technologies: ["JavaScript", "React", "Java"],
+//   },
+//   {
+//     title: "Proyects Name Placeholder",
+//     img: "https://placehold.co/150x150",
+//     description:
+//       "Ponyta is very weak at birth. It can barely stand up. This Pokémon becomes stronger by stumbling and falling to keep up with its parent.It’s proud of its thick horn.",
+//     direction: "#",
+//     technologies: ["JavaScript", "React", "Java"],
+//   },
+// ];
 export const Proyects = () => {
   return (
     <>
